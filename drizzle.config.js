6d397f1eb013caf8fd18975eb981ceaeb8971607
tmampa <tmampa94@gmail.com>
2026-04-30 08:47:52 +0200

    chore: add dotenv dependency and configure environment variable loading in drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
   schema:    './db/schema.js',
@@ -8,3 +10,4 @@ export default defineConfig({
     url: process.env.DATABASE_URL,
   },
 });
+
