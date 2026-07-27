acfe3f188d95c90bb0b03a4ee81cd60e720dec05
tmampa <tmampa94@gmail.com>
2026-07-27 20:45:28 +0200

    chore: add pnpm workspace configuration to allow builds for specific packages

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local', override: true });
 
 export default defineConfig({
   schema:    './db/schema.js',
